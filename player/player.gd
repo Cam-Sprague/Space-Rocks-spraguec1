5382f2b1ca33a05f7bd556e26f5adbebbea33f78
Cam-Sprague <spraguec1@udayton.edu>
2024-10-24 22:44:45 -0400

    Finished player, added Bullet, Main, Rock, and Explosion Scene

--- player/player.gd
+++ player/player.gd
@@ -4,12 +4,18 @@ enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
 @export var engine_power = 500
 @export var spin_power = 8000
+@export var bullet_scene : PackedScene
+@export var fire_rate = .25
 
+var can_shoot = true
 var thrust = Vector2.ZERO
 var rotation_dir = 0
+var screensize = Vector2.ZERO
 
 func _ready():
 	change_state(ALIVE)
+	screensize = get_viewport_rect().size
+	$GunCooldown.wait_time = fire_rate
 	
 func _process(delta):
 	get_input()
@@ -33,7 +39,28 @@ func get_input():
 	if Input.is_action_pressed("thrust"):
 		thrust = transform.x * engine_power
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	if Input.is_action_pressed("shoot"):
+		shoot()
 	
 func _physics_process(delta):
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
+	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrap(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrap(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+	
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start($Muzzle.global_transform)
+
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
